b4623db0f72ac1c49c20b6bf201a55345850669e
Lautaro Ramos <lautaroramos0212@gmail.com>
2024-11-07 09:34:05 -0300

    java

--- scripts/app.js
+++ scripts/app.js
@@ -1,145 +1,43 @@
-// let mostrarcatalogo = () => {
-//   let catalogo = "seleccionar producto a consumir";
-//   catalogo += "1-hamburguesa: $15";
-//   catalogo += "2-gaseosa: $5";
-//   catalogo += "3-papas: $8";
-//   return catalogo;
-// };
+const nombreTarea = document.getElementById("nombreTarea");
+const prioTarea = document.getElementById("prioTarea");
+const fechTarea = document.getElementById("fechTarea");
+const btnAgregar = document.getElementById("btnAgregar");
+const listaTareas = document.getElementById("listaTareas");
+const tareasPendientes = document.getElementById("tareasPendientes");
 
-// let comprar = () => {
-//   let total = 0;
-//   let comprar = true;
-//   let catalogo = mostrarcatalogo;
-//   while (comprar) {
-//     let eleccion = prompt(catalogo + "Elija un producto o terminar para salir");
+const tareas = [];
 
-//     if (eleccion === "" || eleccion === null || isNaN(eleccion)) {
-//       alert("INGRESAR DATO VALIDO");
-//       continue;
-//     }
-//     if (eleccion === "salir") {
-//       break;
-//     }
-//     eleccion = parseInt(eleccion);
-//     if (eleccion < 1 || eleccion > 3) {
-//       alert("INGRESAR UN NUMERO QUE PERTENEZCA AL CATALOGO");
-//       continue;
-//     }
-//     switch (eleccion) {
-//       case 1:
-//         total += 15;
-//         alert("se añadio hamburguesa");
-//         break;
-//       case 2:
-//         total += 5;
-//         alert("se añadio gaseosa");
-//         break;
-//       case 3:
-//         total += 8;
-//         alert("se añadio papas");
-//         break;
-//     }
-//     comprar = prompt("seguir comprando? si/no");
-//     if (comprar === "si") {
-//       continue;
-//     }
-//     if (comprar === "no") {
-//       break;
-//     }
-//   }
-//   alert(`Gracias por su cumpra el total a pagar es $${total}`);
-// };
-// comprar();
-
-// const tareas = [];
-
-// const agregarTarea = ({
-//   nombre,
-//   descipcion,
-//   fechaDeInicio,
-//   plazoDeEntrega,
-// }) => {
-//   tareas.push({ nombre, descipcion, fechaDeInicio, plazoDeEntrega });
-// };
-
-/* const mostrarTareas = () => {
-  let id = 1;
-  let objeto = "";
-  for (i of tareas) {
-    objeto += `
-   Tarea numero: ${id}
-   Nombre: ${i.nombre}
-   Descripcion: ${i.descipcion}
-   Inicio: ${i.fechaDeInicio}
-   Plazo de Entrega: ${i.plazoDeEntrega} dias\n`;
-    id++;
-  }
-  alert(`LIsta de tareas: \n${objeto}`);
+const renderizarTareas = () => {
+  listaTareas.innerHTML = "";
+  tareas.forEach((tareas) => {
+    const itemTarea = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    const infoTarea = document.createElement("span");
+    infoTarea.textContent = `Tarea: ${tareas.nombre}| Prio: ${tareas.prioridad}| Venc: ${tareas.vencimiento}`;
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "Eliminar";
+    itemTarea.append(checkbox);
+    itemTarea.append(infoTarea);
+    itemTarea.append(btnEliminar);
+    listaTareas.append(itemTarea);
+  });
 };
-
-const cargarDatos = () => {
-  let nombre = prompt("Ingresar nombre de la tarea");
-  let descripcion = prompt("Ingresar descripcion de la tarea");
-  let fechaDeInicio = prompt("Ingresar fecha de inicio de la tarea");
-  let plazoDeEntrega = prompt("Ingresar plazo de la tarea");
-  if (nombre && fechaDeInicio && plazoDeEntrega && descripcion) {
-    return { nombre, descripcion, fechaDeInicio, plazoDeEntrega };
+const agregarTarea = () => {
+  const nombre = nombreTarea.value;
+  const prioridad = prioTarea.value;
+  const vencimiento = fechTarea.value;
+  if (nombre && prioridad && vencimiento) {
+    tareas.push({
+      nombre: nombre,
+      prioridad: prioridad,
+      vencimiento: vencimiento,
+      completada: false,
+    });
+    renderizarTareas();
   } else {
-    alert("INGRESA DATOS VALIDOS");
+    alert("Datos invalidos");
   }
 };
-const eliminarTarea = (nombre) => {
-  const i = tareas.findIndex((tareas) => tareas.nombre === nombre);
-  if (i !== -1) {
-    tareas.splice(i, 1);
-    alert(`La tarea ${nombre} se elimino`);
-    alert(tareas);
-  } else {
-    alert(`La tarea ${nombre} no se encuentra en la lista`);
-  }
-};
-
-const buscarTarea = (buscar) => {
-  const i = tareas.findIndex((tareas) => tareas.nombre === buscar);
-  if (i !== -1) {
-    alert(`La tarea ${buscar} se encuentra en la lista`);
-  } else {
-    alert(`La tarea ${buscar} no se encuentra en la lista`);
-  }
-};
-
-const main = () => {
-  let op = "";
-  while (op != 5) {
-    op = prompt(
-      "SELECCIONA UNA OPCION: \n1. Agregar tarea \n2. Ver lista de tareas \n3. Eliminar tarea \n4. Buscar tarea \n5. SALIR "
-    );
-    switch (op) {
-      case "1":
-        let nuevaTarea = cargarDatos();
-        if (nuevaTarea) {
-          agregarTarea(nuevaTarea);
-        }
-        break;
-      case "2":
-        mostrarTareas();
-        break;
-      case "3":
-        mostrarTareas();
-        let e = prompt("Ingrese nombre de tarea a eliminar");
-        eliminarTarea(e);
-        break;
-      case "4":
-        let b = prompt("Ingrese Nombre de la tarea a buscar");
-        buscarTarea(b);
-        break;
-      case "5":
-        alert("Nos vemos!");
-        break;
-      default:
-        alert("Ingrese un dato valido ;)");
-    }
-  }
-};
-main();
- */
+btnAgregar.addEventListener("click", agregarTarea);
+renderizarTareas();
