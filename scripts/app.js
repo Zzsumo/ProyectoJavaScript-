7435e72a95105c0642ac9901496b996feea5d97c
Lautaro Ramos <lautaroramos0212@gmail.com>
2024-11-24 20:13:36 -0300

    pagina de tareas

--- scripts/app.js
+++ scripts/app.js
@@ -13,15 +13,19 @@ const renderizarTareas = () => {
     const itemTarea = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked = tareas.completada;
+    checkbox.addEventListener("change", () => completada(checkbox, tareas.id));
     const infoTarea = document.createElement("span");
-    infoTarea.textContent = `Tarea: ${tareas.nombre}| Prio: ${tareas.prioridad}| Venc: ${tareas.vencimiento}`;
+    infoTarea.innerHTML = ` ${tareas.nombre} | ${tareas.prioridad} | <strong>Venc: ${tareas.vencimiento}</strong>`;
     const btnEliminar = document.createElement("button");
     btnEliminar.textContent = "Eliminar";
+    btnEliminar.addEventListener("click", () => eliminarTarea(tareas.id));
     itemTarea.append(checkbox);
     itemTarea.append(infoTarea);
     itemTarea.append(btnEliminar);
     listaTareas.append(itemTarea);
   });
+  pendientes();
 };
 const agregarTarea = () => {
   const nombre = nombreTarea.value;
@@ -41,5 +45,26 @@ const agregarTarea = () => {
     alert("Datos invalidos");
   }
 };
+const eliminarTarea = (id) => {
+  const ind = tareas.findIndex((tarea) => tarea.id === id);
+  if (ind !== -1) {
+    tareas.splice(ind, 1);
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    renderizarTareas();
+  }
+};
+const pendientes = () => {
+  const pendientes = tareas.filter((tarea) => !tarea.completada).length;
+  tareasPendientes.textContent = `Tareas pendientes:${pendientes}`;
+};
+const completada = (checkbox, id) => {
+  const comp = tareas.find((tarea) => tarea.id === id);
+  if (comp) {
+    comp.completada = checkbox.checked;
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    renderizarTareas();
+  }
+};
+
 btnAgregar.addEventListener("click", agregarTarea);
 renderizarTareas();
